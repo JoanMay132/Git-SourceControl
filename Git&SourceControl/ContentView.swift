356997277988d774960fb86b5445e44e4e54d568
JoanMay132 <jcmd270@gmail.com>
2024-09-28 14:59:17 -0600

    [Feature] Add Suscribe button

--- Git&SourceControl/ContentView.swift
+++ Git&SourceControl/ContentView.swift
@@ -18,6 +18,9 @@ struct ContentView: View {
                 
             }
             .background(.red)
+            Button("Suscribe"){
+                
+            }
         }
         .padding()
     }
